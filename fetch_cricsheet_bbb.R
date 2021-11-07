c09175e9e7a1d5e477bc3b41c1dd06d0ac8fe9e4
Jacquie Tran <jacquietran@users.noreply.github.com>
2021-11-07 13:19:55 +1300

    Complete and working function!

--- fetch_cricsheet_bbb.R
+++ fetch_cricsheet_bbb.R
@@ -1,36 +1,35 @@
-# DONE: Bangladesh Premier League, BBL, Caribbean Premier League, Charlotte Edwards Cup, County Championship,
-# IPL, Mzansi Super League, Pakistan Super League,
-# Rachael Heyhoe Flint Trophy,
-# Sheffield Shield, T20 Blast, WBBL, WIPL (Women's T20 Challenge)
-# ------------------------------------------------------------------------------
-
-# TODO: Super Smash (men and women), The Hundred (men and women)
-#####
-# Formats that don't have an "all_matches.csv" file:
-# Non-official T20I, T20I, One-day matches, ODIs, Multi-day matches, Tests
+# For retrieving ball-by-ball data ---------------------------------------------
 
 fetch_cricsheet_bbb <- function(
   competition = c(
-    "bbl", "bpl", "cpl", "county", "edwards_cup", "heyhoe_flint_trophy", "ipl",
-    "msl", "psl", "sheffield_shield", "t20_blast", "wbbl", "wipl")
-  #sex = c("men", "women")
+    "apl", "bbl", "bpl", "cpl", "county", "edwards_cup", "heyhoe_flint_trophy",
+    "ipl", "multi_day_matches", "msl", "odi_men", "odi_women", "odm_men",
+    "odm_women", "psl", "sheffield_shield", "super_smash_men",
+    "super_smash_women", "tests_men", "tests_women", "the_hundred_men",
+    "the_hundred_women", "t20_blast", "t20_int_men", "t20_int_women",
+    "t20_non_official_men", "t20_non_official_women", "wbbl", "wipl")
   )
 {
-  
-  # Set the value for 'sex' - for some leagues / formats this is essential!
-  # e.g., women's T20I vs. men's T20I
   
   # Define URL based on function arguments
   possible_urls <- data.frame(
     competition_type = c(
-      "bbl", "bpl", "cpl", "county", "edwards_cup", "heyhoe_flint_trophy",
-      "ipl", "msl", "psl", "sheffield_shield", "t20_blast", "wbbl", "wipl"),
+      "apl", "bbl", "bpl", "cpl", "county", "edwards_cup",
+      "heyhoe_flint_trophy", "ipl", "multi_day_matches", "msl", "odi_men",
+      "odi_women", "odm_men", "odm_women", "psl", "sheffield_shield",
+      "super_smash_men", "super_smash_women", "tests_men", "tests_women",
+      "the_hundred_men", "the_hundred_women", "t20_blast", "t20_int_men",
+      "t20_int_women", "t20_non_official_men", "t20_non_official_women",
+      "wbbl", "wipl"),
     url_segment = c(
-      "bbl", "bpl", "cpl", "cch", "cec", "rhf", "ipl", "msl", "psl", "ssh",
-      "ntb", "wbb", "wtc"),
+      "apl", "bbl", "bpl", "cpl", "cch", "cec", "rhf", "ipl", "mdms", "msl",
+      rep("odis", 2), rep("odms", 2), "psl", "ssh", rep("ssm", 2),
+      rep("tests", 2), rep("hnd", 2), "ntb", rep("t20s", 2), rep("it20s", 2),
+      "wbb", "wtc"),
     sex = c(
-      "male", "male", "male", "male", "female", "female", "male", "male",
-      "male", "male", "male", "female", "female")) |>
+      rep("male", 5), rep("female", 2), rep("male", 4), "female", "male",
+      "female", rep("male", 3), "female", "male", "female", "male", "female",
+      rep("male", 2), "female", "male", rep("female", 3))) |>
     dplyr::mutate(
       glued_url = glue::glue(
         "https://cricsheet.org/downloads/{url_segment}_{sex}_csv2.zip"))
@@ -43,12 +42,42 @@ fetch_cricsheet_bbb <- function(
   temp <- tempfile()
   download.file(url, temp)
   
-  # Extract the CSV containing collated match data
-  # and save to specified file directory (filedir arg)
-  unzip(temp, "all_matches.csv", exdir = tempdir())
+  # List all files in zip
+  check_files <- as.character(
+    unzip(temp, exdir = tempdir(), list = TRUE)$Name)
   
-  # Read data from CSV saved locally
-  all_matches <- read.csv(paste0(tempdir(), "/all_matches.csv"))
+  # List all files in zip, excluding:
+  # README.txt and *_info.csv
+  match_files <- as.data.frame(check_files) |>
+    dplyr::mutate(
+      exclude_flag = dplyr::case_when(
+        stringr::str_detect(check_files, "txt")   ~ "exclude",
+        stringr::str_detect(check_files, "_info") ~ "exclude",
+        TRUE                                      ~ "include")) |>
+    dplyr::filter(exclude_flag == "include") |>
+    dplyr::select(-exclude_flag) |>
+    dplyr::pull(check_files)
+  
+  # List match files with full file paths
+  match_filepaths <- file.path(tempdir(), match_files)
+  
+  # If there is an "all_matches.csv" file in the zip
+  # then unzip and read that file in
+  if("all_matches.csv" %in% check_files){
+    # Extract the CSV containing collated match data
+    # and store in a temporary file directory
+    unzip(temp, "all_matches.csv", exdir = tempdir())
+    # Read data from CSV stored in the temp directory
+    all_matches <- read.csv(paste0(tempdir(), "/all_matches.csv"))
+  }
+  # Else unzip the file and read in only the CSV files that
+  # correspond to specific matches
+  else{
+    unzip(temp, match_files, exdir = tempdir())
+    # Read data from multiple CSVs stored in the temp directory
+    all_matches <- do.call(
+      "rbind", lapply(match_filepaths, FUN=function(files){ read.csv(files)}))
+  }
   
   unlink(temp)
   
